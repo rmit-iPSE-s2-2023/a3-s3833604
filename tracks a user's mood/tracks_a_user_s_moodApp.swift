cbac3d70887a328fae5544a6c2959ddd9d87af96
LeiZhaoyy <s3833604@student.rmit.edu.au>
2023-10-24 17:32:42 +1100

    Initial Commit

---
+++ tracks a user's mood/tracks_a_user_s_moodApp.swift
@@ -0,0 +1,17 @@
+//
+//  tracks_a_user_s_moodApp.swift
+//  tracks a user's mood
+//
+//  Created by 赵磊 on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct tracks_a_user_s_moodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
